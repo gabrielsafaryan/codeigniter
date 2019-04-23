dc7ebdf4f4ebb24716af5bb722605d58eee77589
gabrielsafaryan <gabrielsafaryan>
2019-04-23 12:37:05 +0400

    change config.php in github

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://codeigniter.loc/';
+$base_url = '';
+// The regular expression is only a basic validation for a valid "Host" header.
+// It's not exhaustive, only checks for valid characters.
+if (isset($_SERVER['HTTP_HOST']) && preg_match('/^((\[[0-9a-f:]+\])|(\d{1,3}(\.\d{1,3}){3})|[a-z0-9\-\.]+)(:\d+)?$/i', $_SERVER['HTTP_HOST']))
+{
+    $base_url = (is_https() ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST']
+        .substr($_SERVER['SCRIPT_NAME'], 0, strpos($_SERVER['SCRIPT_NAME'], basename($_SERVER['SCRIPT_FILENAME'])));
+}
+else
+{
+    if ((PHP_SAPI === 'cli' OR defined('STDIN')))
+    {
+        global $argc, $argv;
+        if(isset($argv[1]) && $argv[1] == 'ecommerce' && isset($argv[2]) && $argv[2] == 'cron' && isset($argv[3]))
+        {
+            $base_url = $argv[3];
+        }
+    }
+}
+
+
+$config['base_url'] = $base_url;
 
 /*
 |--------------------------------------------------------------------------
